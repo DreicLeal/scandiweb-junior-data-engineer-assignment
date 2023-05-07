54629f960791b7918d60dc0917dd60d72cfc8009
unknown <dreicleal@hotmail.com>
2023-05-07 11:06:20 -0300

    feat: navBar

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,22 +1,26 @@
-import { useState } from "react"
-import { NavigateFunction, useNavigate } from "react-router-dom"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { DropDown } from "./dropDown";
 
 export const NavBar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false)
-    const [isCartOpen, setIsCartOpen] = useState(false)
-    const navigate = useNavigate()
-    return (
-        <header>
-            <ul>
-                <li>WOMEN</li>
-                <li>MEN</li>
-                <li>KIDS</li>
-            </ul>
-            <img src="../assets/a-logo.png" alt="logo" />
-            <div>
-                <button onClick={()=> setIsMenuOpen(!isMenuOpen)}>$</button>
-                <button onClick={() => setIsCartOpen(!isCartOpen)}>{"../assets/cart.png"}</button>
-            </div>
-        </header>
-    )
-}
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const navigate = useNavigate();
+  return (
+    <header>
+      <ul>
+        <li>WOMEN</li>
+        <li>MEN</li>
+        <li>KIDS</li>
+      </ul>
+      <img src="../assets/a-logo.png" alt="logo" />
+      <div>
+        <button onClick={() => setIsMenuOpen(!isMenuOpen)}>$</button>
+        {isMenuOpen && <DropDown />}
+        <button onClick={() => setIsCartOpen(!isCartOpen)}>
+          {"../assets/cart.png"}
+        </button>
+      </div>
+    </header>
+  );
+};
