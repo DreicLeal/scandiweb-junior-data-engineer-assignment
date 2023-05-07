1600c37a708d43e71d1629f8e104df6ec67ee97d
unknown <dreicleal@hotmail.com>
2023-05-07 11:03:48 -0300

    feat: Drop Down

--- src/components/navBar/dropDown.tsx
+++ src/components/navBar/dropDown.tsx
@@ -3,8 +3,8 @@ export const DropDown =() => {
     return (
         <ul>
             <li><button>$ USD</button></li>
-            <li><button></button></li>
-            <li><button></button></li>
+            <li><button>€ EUR</button></li>
+            <li><button>¥ JPY</button></li>
         </ul>
     )
 }
