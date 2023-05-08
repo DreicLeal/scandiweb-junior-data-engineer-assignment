f979276182bdf93253d95746d6db1975de394836
unknown <dreicleal@hotmail.com>
2023-05-08 15:47:40 -0300

    feat: card component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,11 +3,13 @@ import { IProduct } from "../database/databaseInterface";
 import { ProductContext } from "../context/ProductContext";
 import { StyledCardHome } from "../styles/CardHomeStyle";
 import greenIcon from "../assets/greenIcon.svg";
+import { useNavigate } from "react-router-dom";
 
 export const ProductCard = (product: IProduct) => {
   const { currency } = useContext(ProductContext);
+  const navigate = useNavigate()
   return (
-    <StyledCardHome key={product.id}>
+    <StyledCardHome key={product.id} onClick={()=>navigate(`/product/${product.id}`)}>
       <img src={product.img["1"].img1} alt={product.name} />
       <img src={greenIcon} className="addCart" alt="" />
       <div className="productCardInfo">
