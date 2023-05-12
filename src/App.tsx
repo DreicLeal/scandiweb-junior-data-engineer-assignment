56cb37fe6fc1c4f4b336b20b52e626a886b3ad08
unknown <dreicleal@hotmail.com>
2023-05-12 09:47:43 -0300

    tracking home page view

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { BrowserRouter } from "react-router-dom";
 import { NavBar } from "./components/navBar/NavComponent";
 import GlobalStyle from "./styles/GlobalStyle";
 import { ProductProvider } from "./context/ProductContext";
-import ReactGA from "react-ga"
 import { AllRoutes } from "./routes";
 
-const TRACKING_ID = "UA-181755595-1"
-ReactGA.initialize(TRACKING_ID)
+
+
 
 export const App = () => {
+
   return (
     <>
       <GlobalStyle />
